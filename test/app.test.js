df3cd4a15b26d1ff3fc8e4c09bdbf0fffb88331c
Nasir-1310 <bsse1310@iit.du.ac.bd>
2025-01-07 10:51:23 +0600

    test

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  it('should return Hello World2!', async () => {
+  it('should return Hello', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello Bangladesh');

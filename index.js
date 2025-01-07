525e40d0821cdd5ef6b2720063dd740fe2efea65
Nasir-1310 <bsse1310@iit.du.ac.bd>
2025-01-07 11:09:23 +0600

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Bangladesh');
+    res.send('Hello world');
 });
 
 module.exports=app;
--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
   it('should return Hello', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello Bangladesh');
+    expect(res.text).toBe('Hello world');
   });
 
 
